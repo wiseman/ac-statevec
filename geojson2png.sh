29f6eb2e156e61b29b1fd8d520bd933e509a7e7a
John Wiseman <jjwiseman@gmail.com>
2019-12-02 09:36:12 -0800

    Handle multiple files.

--- geojson2png.sh
+++ geojson2png.sh
@@ -2,9 +2,10 @@
 
 WIDTH=1920
 HEIGHT=1080
-
-INPUT="$1"
-
-geoproject "d3.geoConicEqualArea().parallels([34, 40.5]).rotate([120, 0]).fitSize([$WIDTH, $HEIGHT], d)" < "$INPUT" > /tmp/$$.json
-geo2svg -w 960 -h 960 < /tmp/$$.json > /tmp/$$.svg
-convert /tmp/$$.svg ${INPUT}.png
+for INPUT in "$@"
+do
+    geoproject "d3.geoConicEqualArea().parallels([34, 40.5]).scale(0.5).rotate([120, 0]).fitSize([$WIDTH, $HEIGHT], d)" < "$INPUT" > /tmp/$$.json
+    geo2svg -w $WIDTH -h $HEIGHT < /tmp/$$.json > /tmp/$$.svg
+    convert /tmp/$$.svg ${INPUT}.png
+    echo Rendered ${INPUT}.png
+done
